eee591ad200c9051c5cf3ef286e189f3ed960f57
heyushuo <1007561607@qq.com>
2017-08-15 14:51:17 +0800

    add axios

--- boss/src/main.js
+++ boss/src/main.js
@@ -3,12 +3,17 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
+import axios from 'axios'
 // rem
 import "./js/flexible"
 //导入字体样式
 import "./styles/fontstyle.css"
 import "./styles/base.scss"
+
+
+
+Vue.prototype.$http = axios  //其他页面在使用axios的时候直接  this.$http就可以了
+
 
 
 Vue.config.productionTip = false
